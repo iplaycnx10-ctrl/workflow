37827965e276dfb182c6a29db86a6d66a238585d
iplaycnx10-ctrl <iplaycnx.10@gmail.com>
2026-05-22 07:00:11 +0700

    Wire six action plan subtabs

--- ui/action_plan.py
+++ ui/action_plan.py
@@ -1,14 +1,37 @@
 import streamlit as st
 
+from ui.action_plan_ldp import render_ldp_plan_tab
+from ui.action_plan_pixel import render_pixel_plan_tab
+from ui.action_plan_ga4 import render_ga4_plan_tab
+from ui.action_plan_dropoff import render_dropoff_plan_tab
+from ui.action_plan_case import render_case_plan_tab
+from ui.action_plan_stability import render_stability_plan_tab
+
 
 def render_action_plan_module():
-    st.markdown(
-        """
-        <div class="action-plan-placeholder">
-            <span class="action-plan-kicker">ACTION PLAN</span>
-            <h3>แผนปฏิบัติการ</h3>
-            <p>พื้นที่สำหรับลงรายละเอียดแผนการทำงาน ขั้นตอนการยิงแอด ครีเอทีฟที่ต้องใช้ Timeline และ Checklist เพิ่มเติม</p>
-        </div>
-        """,
-        unsafe_allow_html=True,
-    )
+    tabs = st.tabs([
+        "01 ปรับ LDP",
+        "02 Pixel",
+        "03 GA4",
+        "04 Drop-off",
+        "05 Case 1 วัน",
+        "06 ระบบเสถียร",
+    ])
+
+    with tabs[0]:
+        render_ldp_plan_tab()
+
+    with tabs[1]:
+        render_pixel_plan_tab()
+
+    with tabs[2]:
+        render_ga4_plan_tab()
+
+    with tabs[3]:
+        render_dropoff_plan_tab()
+
+    with tabs[4]:
+        render_case_plan_tab()
+
+    with tabs[5]:
+        render_stability_plan_tab()
